4ee80915a74620c9ddaa1fd04617a494000a45ae
Bruce <bruce@sum-it.com>
2014-08-22 19:04:56 -0700

    fix user/passwoed fields

--- cfg_dbAccess.py
+++ cfg_dbAccess.py
@@ -1,11 +1,6 @@
 dbAccess = {
-<<<<<<< HEAD
     'user': 'sumit',
-    'password': '*******',
-=======
-    'user': '******',
-    'password': '******',
->>>>>>> 4183044c1773ce9022e5b2175e0de47272e83fae
+    'password': 'f1sumit',
     'host': '192.168.51.80',
     'database': 'marshdta',
     'raise_on_warnings': True
